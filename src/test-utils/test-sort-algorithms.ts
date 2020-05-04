ed642fbbd9fc70564f6e27c02a68a2c27fb174c1
MICHAEL <michaeljfei@163.com>
2020-05-04 20:37:54 +0800

    test: 💍 更新test测试用例命名
    更新test测试用例命名
    
    BREAKING CHANGE: 🧨 更新test测试用例命名

--- src/test-utils/test-sort-algorithms.ts
+++ src/test-utils/test-sort-algorithms.ts
@@ -1,6 +1,6 @@
 export function testSortAlgorithms(sortAlgorithm: Function, algorithmName: string) {
   describe(algorithmName, () => {
-    let norSortArray = [13, 45, 2, 76, 32, 90, 5, 31];
+    const noSortArray = [13, 45, 2, 76, 32, 90, 5, 31];
     const sortedArray = [2, 5, 13, 31, 32, 45, 76, 90];
 
     it(`${algorithmName}--测试空数组`, () => {
@@ -8,7 +8,7 @@ export function testSortAlgorithms(sortAlgorithm: Function, algorithmName: strin
     });
 
     it(`${algorithmName}--测试无序数组能否正常排序`, () => {
-      expect(sortAlgorithm(norSortArray)).toEqual(sortedArray);
+      expect(sortAlgorithm(noSortArray)).toEqual(sortedArray);
     });
 
     it(`${algorithmName}--测试有序数组能否正常排序`, () => {
