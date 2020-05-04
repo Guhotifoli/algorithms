35941dad065a5a9cca6ac63aee9aa8a2750124b2
MICHAEL <michaeljfei@163.com>
2020-05-04 21:33:12 +0800

    docs: ✏️ 补充选择排序实现过程注释
    补充选择排序实现过程注释
    
    BREAKING CHANGE: 🧨 补充选择排序实现过程注释

--- src/sort/selection-sort/index.ts
+++ src/sort/selection-sort/index.ts
@@ -11,15 +11,18 @@ export function selectionSort<T>(array: T[], compareFn = compareBiggerThan): T[]
   let minElementIndex: number;
   const length = array.length;
 
+  // 开始外层循环，因为有最后一轮比较，外层循环不遍历到最后一个元素
   for (let i = 0; i < length - 1; i++) {
+    // 先将最小值的位置指向每次外层循环的索引
     minElementIndex = i;
-
+    // 开始内层循环，内层循环
     for (let j = i; j < length; j++) {
+      // 找到内层循环的当前最小值的位置
       if (compareFn(array[minElementIndex], array[j])) {
         minElementIndex = j;
       }
     }
-
+    // 判断当前循环的起点是否为最小值的位置，如果不是就交换位置
     if (i !== minElementIndex) {
       swap(i, minElementIndex, array);
     }
