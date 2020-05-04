b5a3518a981c98690292b73c22554c8d38ad343b
MICHAEL <michaeljfei@163.com>
2020-05-04 18:19:01 +0800

    test: 💍 判断函数测试用例
    判断函数测试用例
    
    BREAKING CHANGE: 🧨 判断函数测试用例

---
+++ src/helpers/__tests__/compare.spec.ts
@@ -0,0 +1,26 @@
+
+import { compareLessThan, compareEqual, compareBiggerThan } from '../compare';
+
+describe('测试默认比较函数', () => {
+  it('测试小于函数', () => {
+    expect(compareLessThan(-1, 1)).toEqual(true);
+    expect(compareLessThan(0, 1)).toEqual(true);
+    expect(compareLessThan(1, -1)).toEqual(false);
+    expect(compareLessThan(1, 1)).toEqual(false);
+    expect(compareLessThan(-1, -1)).toEqual(false);
+  });
+
+  it('测试等于函数', () => {
+    expect(compareEqual(-1, 1)).toEqual(false);
+    expect(compareEqual(0, 1)).toEqual(false);
+    expect(compareEqual(0, 0)).toEqual(true);
+  });
+
+  it('测试大于函数', () => {
+    expect(compareBiggerThan(-1, 1)).toEqual(false);
+    expect(compareBiggerThan(0, 1)).toEqual(false);
+    expect(compareBiggerThan(1, -1)).toEqual(true);
+    expect(compareLessThan(1, 1)).toEqual(false);
+    expect(compareLessThan(-1, -1)).toEqual(false);
+  });
+})
